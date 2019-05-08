a3fe7fbe1f43710fd49d6915e596af202ddc10fc
Thiago Ferraz Vieira da Cunha <thiago.cunha@dataprev.gov.br>
2019-05-08 14:27:59 -0300

    first commit

---
+++ src/main/java/com/neueda/littleurl/LittleUrlApplication.java
@@ -0,0 +1,13 @@
+package com.neueda.littleurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleUrlApplication.class, args);
+	}
+
+}
